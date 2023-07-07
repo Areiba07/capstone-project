b7f42f9cb776a78fcbec7ea40452dba0a820e873
John Kyle Joseph Reyes <jkj313.kr@gmail.com>
2023-07-07 19:26:55 +0800

    generic (static) carousel

--- hanapbuhay/src/pages/root.jsx
+++ hanapbuhay/src/pages/root.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/header";
+import Carousel from "../components/carousel";
 
 export default function Root() {
   return (
     <>
       <Header/>
+      <Carousel/>
     </>
   );
 }
