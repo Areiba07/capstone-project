94280dd8f4a6d328302e55b8ac8c964eb9679c00
LA Abiera <130994662+Areiba07@users.noreply.github.com>
2023-07-24 19:21:52 +0800

    Renamed export function

--- hanapbuhay/src/pages/login.jsx
+++ hanapbuhay/src/pages/login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Root() {
+export default function Login() {
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
       <div className="w-full p-6 m-auto bg-blue-50 rounded-md shadow-md lg:max-w-xl">
