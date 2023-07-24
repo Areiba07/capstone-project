9300a0d3c4971adbc33ae6d7e0ec84cac6eb09f1
LA Abiera <130994662+Areiba07@users.noreply.github.com>
2023-07-24 19:19:45 +0800

    Created footer component

---
+++ hanapbuhay/src/components/footer.jsx
@@ -0,0 +1,49 @@
+
+import { Footer } from 'flowbite-react';
+import Logo from "../components/logo";
+
+
+
+export default function FooterWithLogo() {
+  return (
+    <Footer container>
+      <div className="w-full text-center">
+        <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+          
+          <div className="flex items-center">
+            <Logo
+            
+            alt="Flowbite Logo"
+            href="https://flowbite.com"
+            name="Flowbite"
+            src="https://flowbite.com/docs/images/logo.svg"
+            /> 
+            <h1 className=" text-purple-700 font-bold ">HanapBuhay</h1>
+          </div>
+          <Footer.LinkGroup>
+            <Footer.Link href="#">
+              About
+            </Footer.Link>
+            <Footer.Link href="#">
+              Privacy Policy
+            </Footer.Link>
+            <Footer.Link href="#">
+              Licensing
+            </Footer.Link>
+            <Footer.Link href="#">
+              Contact
+            </Footer.Link>
+          </Footer.LinkGroup>
+        </div>
+        <Footer.Divider />
+        <Footer.Copyright
+          by="HanapBuhay"
+          href="#"
+          year={2023}
+        />
+      </div>
+    </Footer>
+  )
+}
+
+
