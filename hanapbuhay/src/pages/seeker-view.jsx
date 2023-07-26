a90a57b2a69665ba46f01b57942a90ccb938b32b
John Kyle Joseph Reyes <jkj313.kr@gmail.com>
2023-07-26 16:10:28 +0800

    added initial view request
    added initial view request

--- hanapbuhay/src/pages/seeker-view.jsx
+++ hanapbuhay/src/pages/seeker-view.jsx
@@ -19,6 +19,19 @@ const UserPage = () => {
           </div>
         </div>
       </div>
+      <div class="flex items-center justify-center h-screen">
+        <div class="w-500 h-300 bg-white shadow-lg rounded-lg overflow-hidden z-50">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEIqTA-V654XJcRmVOOTRG4-GONkgIRslerTMVM9wIg_A74i0k1ZzZmO5Z3DKiRx89-9E&usqp=CAU"
+            alt="Photo"
+            class="w-full h-40 object-cover"
+          />
+          <div class="p-4">
+            <h2 class="text-xl font-semibold">Request Title</h2>
+            <p class="text-gray-600 mt-2">Request Description</p>
+          </div>
+        </div>
+      </div>
       <div>{<Outlet />}</div>
       <FooterWithLogo />
     </>
