9234ec42730f17cd523871639567db1d682592da
LA Abiera <130994662+Areiba07@users.noreply.github.com>
2023-07-27 21:56:52 +0800

    Additional assets for Login page

--- hanapbuhay/src/pages/login.jsx
+++ hanapbuhay/src/pages/login.jsx
@@ -1,6 +1,8 @@
 import { Button } from "flowbite-react";
 import { Link } from "react-router-dom";
 import HanapBuhayLogo from "../assets/images/logo.png";
+import seekerLogo from "../assets/images/seeker2.svg"
+import providerLogo from "../assets/images/provider2.svg"
 import 'tailwindcss/tailwind.css';
 
 export default function SignUp() {
@@ -16,51 +18,37 @@ export default function SignUp() {
           <h1 className="text-center text-4xl font-bold text-gray-800 mb-6">
             Welcome to HanapBuhay!
           </h1>
+          <h2 className="text-center text-4xl font-bold text-gray-800 mb-6">Login as:</h2>
         </div>
         <br />
-        <div className=" mx-20 text-justify justify-around flex flex-col md:flex-row  mb-8">
-            <div className="justify-between md:w-2/5 bg-violet-500 bg-opacity-70 p-6 rounded-lg mb-4">
-              <h2 className="text-2xl font-bold text-gray-800 mb-4">
-                For Service Providers
+        <div className=" mx-20 text-justify  justify-around flex flex-col md:flex-row  mb-8">
+            <div className="justify-between md:w-2/4 bg-opacity-70 p-6 rounded-lg mb-4">
+              <h2 className="text-2xl text-center font-bold text-gray-800 mb-4">
+                Service Providers
               </h2>
-              <p className="text-lg text-gray-700">
-                Discover a wide range of service opportunities from various
-                industries, all in one place. Our intelligent matching algorithm
-                analyzes your skills, qualifications, and preferences to present
-                you with relevant and personalized service listings. Whether
-                you're a freelancer, contractor, or business owner, HanapBuhay
-                has got you covered. Take advantage of our advanced search
-                filters, showcase your portfolio, and connect with service
-                seekers looking for your expertise.
-              </p>
+
+              <Link to={"/providerLogin"}>              
+              <img
+                  alt="providerLogo"
+                  className=" hover:bg-indigo-200 rounded-full mx-auto justify-normal w-100 h-100"
+                  src={providerLogo}
+              />
+              </Link>
+
               <br />
-              <div>
-                <Button gradientDuoTone="purpleToPink" pill>
-                  <Link to={"/providerLogin"}>LOGIN AS PROVIDER</Link>
-                </Button>
-              </div>
             </div>
-            <div className="md:w-2/5 bg-blue-400 bg-opacity-80 p-6 rounded-lg mb-4">
-              <h2 className="text-2xl font-bold text-gray-800 mb-4">
-                For Service Seekers
+            <div className="md:w-2/4 bg-opacity-80 p-6 rounded-lg mb-4">
+              <h2 className="text-2xl text-center font-bold text-gray-800 mb-4">
+               Service Seekers
               </h2>
-              <p className="text-lg text-gray-700">
-                Finding the right service provider for your needs is crucial,
-                and we're here to make the process efficient and effective.
-                HanapBuhay provides a powerful platform that enables you to
-                search and connect with qualified service providers. Post your
-                service requirements, browse through profiles, and engage with
-                service providers who meet your criteria. Our platform also
-                offers seamless communication channels, allowing you to discuss
-                project details, negotiate terms, and manage the hiring process
-                with ease.
-              </p>
-              <br />
-              <div>
-                <Button gradientDuoTone="purpleToPink" pill>
-                  <Link to={"/seekerLogin"}>LOGIN AS SEEKER</Link>
-                </Button>
-              </div>
+              <Link to={"/providerLogin"}>              
+              <img
+                  alt="providerLogo"
+                  className=" hover:bg-indigo-200 rounded-full  mx-auto justify-normal w-100 h-100"
+                  src={seekerLogo}
+              />
+              </Link>
+
             </div>
           </div>
       </div>
