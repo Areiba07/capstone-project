04f96c8b9db140d61f80c1a2bc178dd351a8650f
John Kyle Joseph Reyes <jkj313.kr@gmail.com>
2023-07-28 11:26:30 +0800

    removed href to flowbite
    removed href to flowbite on headers

--- hanapbuhay/src/components/header.jsx
+++ hanapbuhay/src/components/header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
       <header>
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
           <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center">
+            <a href="#" class="flex items-center">
               <Logo />
               <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                 HanapBuhay
