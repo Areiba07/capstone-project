72f591ddbcbcf35bb29b89c381939b9b39a9ef09
LA Abiera <130994662+Areiba07@users.noreply.github.com>
2023-08-31 17:55:31 +0800

    Nav bar link to router

--- hanapbuhay/src/components/navbar-main.jsx
+++ hanapbuhay/src/components/navbar-main.jsx
@@ -1,61 +1,43 @@
-'use client';
-
-import { Button, Navbar } from 'flowbite-react';
-import Logo from "../components/logo";
-
+import React from 'react'; // Make sure to import React
+import { Button, Navbar } from 'flowbite-react'; // Assuming you have 'flowbite-react' installed
+import { Link } from 'react-router-dom'; // Assuming you're using react-router-dom for routing
+import Logo from "../components/logo"; // Make sure this path is correct
 
 export default function NavbarWithCTAButton() {
   return (
-
-    
-    <Navbar
-      fluid
-      rounded
-    >
-      <Navbar.Brand href="/home">
+    <Navbar fluid rounded>
+      <Navbar.Brand as={Link} to="/home"> {/* Use 'as={Link}' for routing */}
         <Logo />
         <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
           HanapBuhay
         </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Button href="/login" gradientDuoTone="purpleToPink">
+        <Button as={Link} to="/login" gradientDuoTone="purpleToPink"> {/* Use 'as={Link}' for routing */}
           Login
         </Button>
-        <br />
         <span className="self-center text-md font-semibold mr-2 ml-2 dark:text-white">
           OR
         </span>
-        <br />
-        <Button href="/signup" gradientDuoTone="purpleToPink" className="mr-2">
-        SignUp
+        <Button as={Link} to="/signup" gradientDuoTone="purpleToPink" className="mr-2"> {/* Use 'as={Link}' for routing */}
+          SignUp
         </Button>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link
-          active
-          href=""
-        >
-          <p>
-            Home
-          </p>
-        </Navbar.Link>
-        <Navbar.Link href="/home">
+        <Navbar.Link as={Link} to="/home"> {/* Use 'as={Link}' for routing */}
           Home
         </Navbar.Link>
-        <Navbar.Link href="/about">
+        <Navbar.Link as={Link} to="/about"> {/* Use 'as={Link}' for routing */}
           About
         </Navbar.Link>        
-        <Navbar.Link href="/service">
+        <Navbar.Link as={Link} to="/service"> {/* Use 'as={Link}' for routing */}
           Services
         </Navbar.Link>
-        <Navbar.Link href="/serviceBoard">
+        <Navbar.Link as={Link} to="/serviceBoard"> {/* Use 'as={Link}' for routing */}
           Service Board
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   )
 }
-
-
