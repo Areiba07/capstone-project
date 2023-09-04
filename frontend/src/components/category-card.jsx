9996c699587925bf4c2ce6c44589902535284132
LA Abiera <130994662+Areiba07@users.noreply.github.com>
2023-09-04 17:15:22 +0800

    Card Design Change

--- frontend/src/components/category-card.jsx
+++ frontend/src/components/category-card.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Button, Card } from 'flowbite-react'; 
 
 const CategoryCard = ({ title, description, imageUrl }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-4">
+    <Card className="flex max-w-m">
       <img src={imageUrl} alt={title} className="w-24 h-24 mx-auto mb-4" />
-      <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
-      <p className="text-gray-600">{description}</p>
-    </div>
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {title}
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        {description}
+      </p>
+      <Button gradientDuoTone="pinkToOrange">
+        <p>
+          Explore
+        </p>
+      </Button>
+    </Card>
   );
 };
 
