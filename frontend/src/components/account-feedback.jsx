484963c018acf2ef4584d20fe9f5ac9c9c384f09
John Kyle Joseph Reyes <jkj313.kr@gmail.com>
2023-09-05 14:31:56 +0800

    User account content update
    added account booking, feedback, messages, and notification content

--- frontend/src/components/account-feedback.jsx
+++ frontend/src/components/account-feedback.jsx
@@ -1,7 +1,204 @@
-export default function AccountFeedback() {
+import React, { useState } from 'react';
+
+// Helper function to generate star icons based on the rating
+const renderStars = (starRating) => {
+  const roundedRating = Math.min(Math.round(starRating), 5); // Limit to a maximum of 5 stars
+  const stars = [];
+
+  for (let i = 0; i < 5; i++) {
+    if (i < roundedRating) {
+      stars.push(
+        <span key={i} className="text-yellow-500">&#9733;</span>
+      ); // Full star
+    } else {
+      stars.push(
+        <span key={i} className="text-gray-400">&#9733;</span>
+      ); // Empty star
+    }
+  }
+
+  return stars;
+};
+
+const ServiceReviewCard = ({ serviceName, starRating, comment }) => {
   return (
-    <>
-        <h1>Account Feedback</h1>
-    </>
+    <div className="w-full bg-white rounded-lg shadow-md p-4">
+      <h3 className="text-xl font-semibold text-gray-800">{serviceName}</h3>
+      <div className="flex items-center text-yellow-500">
+        {renderStars(starRating)}
+      </div>
+      <p className="text-gray-600 mt-2">Comment: {comment}</p>
+    </div>
   );
-}
+};
+
+const AccountFeedback = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedService, setSelectedService] = useState('');
+  const [starRating, setStarRating] = useState(0);
+  const [comment, setComment] = useState('');
+  const [feedbackList, setFeedbackList] = useState([]);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleAddFeedback = () => {
+    // Create a new feedback object
+    const newFeedback = {
+      service: selectedService,
+      rating: parseFloat(starRating),
+      comment,
+    };
+
+    // Add the new feedback to the feedbackList array
+    setFeedbackList([...feedbackList, newFeedback]);
+
+    // Reset form fields
+    setSelectedService('');
+    setStarRating(0);
+    setComment('');
+
+    closeModal();
+  };
+
+  const reviews = [
+    {
+      serviceName: 'Jokjak Plumbing',
+      starRating: 4.5,
+      comment: 'Great service, highly recommended!',
+    },
+    {
+      serviceName: 'Maria Gardening',
+      starRating: 3.0,
+      comment: 'Decent service, could be improved.',
+    },
+    {
+      serviceName: 'Nena Pedicure',
+      starRating: 5.0,
+      comment: 'Exceptional service, exceeded expectations!',
+    },
+  ];
+
+  return (
+    <div className="bg-gray-100 min-h-screen py-8">
+      <h1 className="text-3xl font-semibold text-center text-gray-800 mb-4">
+        Service Reviews
+      </h1>
+      <button
+        onClick={openModal}
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg"
+      >
+        Add Feedback
+      </button>
+      {feedbackList.map((feedback, index) => (
+        <ServiceReviewCard
+          key={index}
+          serviceName={feedback.service}
+          starRating={feedback.rating}
+          comment={feedback.comment}
+        />
+      ))}
+      {reviews.map((review, index) => (
+        <ServiceReviewCard
+          key={index}
+          serviceName={review.serviceName}
+          starRating={review.starRating}
+          comment={review.comment}
+        />
+      ))}
+      <div>
+        {/* Feedback Modal */}
+        <div
+          className={`${
+            isModalOpen ? 'block' : 'hidden'
+          } fixed z-10 inset-0 overflow-y-auto`}
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="bg-white w-1/2 p-4 rounded-lg shadow-xl">
+              <div className="flex justify-between">
+                <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+                  Add Feedback
+                </h2>
+                <button
+                  onClick={closeModal}
+                  className="text-gray-500 hover:text-gray-700 focus:outline-none"
+                >
+                  Close
+                </button>
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="service"
+                  className="block text-gray-700 font-bold"
+                >
+                  Select Service:
+                </label>
+                <select
+                  id="service"
+                  value={selectedService}
+                  onChange={(e) => setSelectedService(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-500"
+                >
+                  <option value="" disabled>
+                    Select a service
+                  </option>
+                  <option value="Service 1">Service 1</option>
+                  <option value="Service 2">Service 2</option>
+                  <option value="Service 3">Service 3</option>
+                </select>
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="rating"
+                  className="block text-gray-700 font-bold"
+                >
+                  Star Rating:
+                </label>
+                <input
+                  type="number"
+                  id="rating"
+                  value={starRating}
+                  onChange={(e) => setStarRating(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-500"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="comment"
+                  className="block text-gray-700 font-bold"
+                >
+                  Comment:
+                </label>
+                <textarea
+                  id="comment"
+                  rows="4"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-500"
+                ></textarea>
+              </div>
+              <div className="flex justify-end">
+                <button
+                  onClick={handleAddFeedback}
+                  className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg"
+                >
+                  Submit Feedback
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountFeedback;
--- frontend/src/components/account-notifications.jsx
+++ frontend/src/components/account-notifications.jsx
@@ -1,8 +1,98 @@
-export default function AccountNotification() {
-    return (
-      <>
-          <h1>Account Notification</h1>
-      </>
-    );
-  }
-  
+import React, { useState } from 'react';
+
+const NotificationTable = () => {
+  const [modalOpen, setModalOpen] = useState(false); // State for modal visibility
+  const [selectedNotification, setSelectedNotification] = useState(null); // State to track selected notification
+  const [notifications, setNotifications] = useState([
+    {
+      id: 1,
+      message: 'New message received',
+      date: '2023-09-05',
+      status: 'unread',
+    },
+    {
+      id: 2,
+      message: 'Pending service request "Manong John Gardening Approved"',
+      date: '2023-09-04',
+      status: 'unread',
+    },
+    {
+      id: 3,
+      message: 'Event reminder: Free Financial Literact Training',
+      date: '2023-09-03',
+      status: 'read',
+    },
+  ]);
+
+  // Function to open the modal and mark notification as read
+  const openModal = (notification) => {
+    setSelectedNotification(notification);
+    setModalOpen(true);
+
+    // Mark the notification as read
+    if (notification.status === 'unread') {
+      const updatedNotifications = [...notifications];
+      const notificationIndex = updatedNotifications.findIndex((n) => n.id === notification.id);
+      if (notificationIndex !== -1) {
+        updatedNotifications[notificationIndex].status = 'read';
+        setNotifications(updatedNotifications);
+      }
+    }
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    setSelectedNotification(null);
+    setModalOpen(false);
+  };
+
+  return (
+    <div className="container mx-auto mt-10">
+      <table className="min-w-full border-collapse">
+        <thead>
+          <tr>
+            <th className="w-1/6 py-2 text-left">Status</th>
+            <th className="w-1/2 py-2 text-left">Message</th>
+            <th className="w-1/6 py-2 text-left">Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {notifications.map((notification) => (
+            <tr
+              key={notification.id}
+              className={notification.status === 'unread' ? 'bg-yellow-100 cursor-pointer' : 'cursor-pointer'}
+              onClick={() => openModal(notification)}
+            >
+              <td className="py-2">{notification.status}</td>
+              <td className="py-2">{notification.message}</td>
+              <td className="py-2">{notification.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* Notification Modal */}
+      {selectedNotification && (
+        <div className={`fixed inset-0 flex items-center justify-center z-50 ${modalOpen ? 'block' : 'hidden'}`}>
+          <div className="modal-overlay fixed inset-0 bg-black opacity-50"></div>
+          <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+            <div className="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50" onClick={closeModal}>
+              <svg className="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                <path d="M18 1.5l-1.5-1.5-7.5 7.5-7.5-7.5-1.5 1.5 7.5 7.5-7.5 7.5 1.5 1.5 7.5-7.5 7.5 7.5 1.5-1.5-7.5-7.5z"></path>
+              </svg>
+            </div>
+
+            {/* Modal Content */}
+            <div className="modal-content py-4 text-left px-6">
+              <h1 className="text-2xl font-medium mb-4">{selectedNotification.message}</h1>
+              <p className="text-gray-600 text-sm mb-4">Date: {selectedNotification.date}</p>
+              <p className="text-gray-600 text-sm">Status: {selectedNotification.status}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NotificationTable;
